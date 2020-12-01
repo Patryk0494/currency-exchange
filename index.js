ac37f46d99fe86ac708035b6c01656f015d74cc2
Patryk <jpatryk0494@gmail.com>
2020-12-01 10:18:37 +0100

    app improved visuali

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
-// axios.get('http://api.nbp.pl/api/exchangerates/rates/C/CHF/today/').then(response => {
-//     console.log("response: ", response);
-// });
-
-// fetch("http://api.nbp.pl/api/exchangerates/rates/C/CHF/today/").then(response => response.json())
-// .then( function(response) {
-//     console.log("response: ", response);
-//     console.log((response.rates[0].ask));
-// }).catch(error => {
-//     console.log("error: ", error);
-// });
 
 const amountInput = document.getElementById("amount-input");
 const convertBtn = document.getElementById("convertBtn");
 const currencySelect = document.getElementById("currency-select");
 const amountOutput = document.getElementById("amount-output")
+const loader = document.getElementById("loader");
 
 convertBtn.addEventListener("click", function() {
     const currency = currencySelect.value;
@@ -22,10 +12,13 @@ convertBtn.addEventListener("click", function() {
     
     fetch(`http://api.nbp.pl/api/exchangerates/rates/C/${currency}/today/`).then(response => response.json())
     .then( function(response) {
+        loader.style.display = "block";
         rateExchange = response.rates[0].ask;
         let sum = convert(amount, rateExchange);
-        amountOutput.innerText = `Po przeliczniu na PLN to ${sum.toFixed(2)} zł.`;
-        // console.log(sum.toFixed(2));
+        setTimeout(function() {
+            loader.style.display = "none";
+            amountOutput.innerText = `Po przeliczniu na PLN to ${sum.toFixed(2)} zł.`;
+        }, 1300)
     }).catch(error => {
         console.log("error: ", error);
     });
